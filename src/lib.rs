4aa58b0bdb73e6c1c965059d0913b2e3bee85340
loyispa <litiegen456250@gmail.com>
2023-05-04 22:11:44 +0800

    improve usability

--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,66 @@
 mod strict_math;
 
-pub use strict_math::racos;
-pub use strict_math::rasin;
-pub use strict_math::ratan;
-pub use strict_math::ratan2;
-pub use strict_math::rcbrt;
-pub use strict_math::rcosh;
-pub use strict_math::rexpm1;
-pub use strict_math::rhypot;
-pub use strict_math::rlog1p;
-pub use strict_math::rsinh;
-pub use strict_math::rtan;
-pub use strict_math::rtanh;
+pub use strict_math::acos;
+pub use strict_math::asin;
+pub use strict_math::atan;
+pub use strict_math::atan2;
+pub use strict_math::cbrt;
+pub use strict_math::cosh;
+pub use strict_math::expm1;
+pub use strict_math::hypot;
+pub use strict_math::log1p;
+pub use strict_math::sinh;
+pub use strict_math::tan;
+pub use strict_math::tanh;
 
 mod test {
     use super::*;
 
     #[test]
     pub fn test() {
-        unsafe {
-            assert_eq!(
-                racos(0.055605003447049994_f64),
-                0.055605003447049994_f64.acos()
-            );
-            assert_eq!(
-                rasin(0.055605003447049994_f64),
-                0.055605003447049994_f64.asin()
-            );
-            assert_eq!(
-                ratan(0.055605003447049994_f64),
-                0.055605003447049994_f64.atan()
-            );
-            assert_eq!(ratan2(3.0_f64, -3.0_f64), 3.0_f64.atan2(-3.0_f64));
-            assert_eq!(
-                rcbrt(0.055605003447049994_f64),
-                0.055605003447049994_f64.cbrt()
-            );
-            assert_eq!(
-                rcosh(0.055605003447049994_f64),
-                0.055605003447049994_f64.cosh()
-            );
-            assert_eq!(
-                rexpm1(0.055605003447049994_f64),
-                0.055605003447049994_f64.exp_m1()
-            );
-            assert_eq!(rhypot(2.0_f64, 3.0_f64), 2.0_f64.hypot(3.0_f64));
-            assert_eq!(
-                rlog1p(0.055605003447049994_f64),
-                0.055605003447049994_f64.ln_1p()
-            );
-            assert_eq!(
-                rsinh(0.055605003447049994_f64),
-                0.055605003447049994_f64.sinh()
-            );
-            assert_eq!(
-                rtan(0.055605003447049994_f64),
-                0.055605003447049994_f64.tan()
-            );
-            assert_eq!(
-                rtanh(0.055605003447049994_f64),
-                0.055605003447049994_f64.tanh()
-            );
-            assert_eq!(rcbrt(-0.055605003447049994_f64), -0.3816845880251514);
-        }
+        assert_eq!(
+            acos(0.055605003447049994_f64),
+            0.055605003447049994_f64.acos()
+        );
+        assert_eq!(
+            asin(0.055605003447049994_f64),
+            0.055605003447049994_f64.asin()
+        );
+        assert_eq!(
+            atan(0.055605003447049994_f64),
+            0.055605003447049994_f64.atan()
+        );
+        assert_eq!(atan2(3.0_f64, -3.0_f64), 3.0_f64.atan2(-3.0_f64));
+        assert_eq!(
+            cbrt(0.055605003447049994_f64),
+            0.055605003447049994_f64.cbrt()
+        );
+        assert_eq!(
+            cosh(0.055605003447049994_f64),
+            0.055605003447049994_f64.cosh()
+        );
+        assert_eq!(
+            expm1(0.055605003447049994_f64),
+            0.055605003447049994_f64.exp_m1()
+        );
+        assert_eq!(hypot(2.0_f64, 3.0_f64), 2.0_f64.hypot(3.0_f64));
+        assert_eq!(
+            log1p(0.055605003447049994_f64),
+            0.055605003447049994_f64.ln_1p()
+        );
+        assert_eq!(
+            sinh(0.055605003447049994_f64),
+            0.055605003447049994_f64.sinh()
+        );
+        assert_eq!(
+            tan(0.055605003447049994_f64),
+            0.055605003447049994_f64.tan()
+        );
+        assert_eq!(
+            tanh(0.055605003447049994_f64),
+            0.055605003447049994_f64.tanh()
+        );
+        eprintln!("cbrt:: {}", -0.055605003447049994_f64.cbrt());
+        assert_eq!(cbrt(-0.055605003447049994_f64), -0.3816845880251514);
     }
 }
