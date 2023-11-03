05b82585df91b9209d2c37968ca8940765b75a41
loyispa <litiegen456250@gmail.com>
2023-11-03 17:39:07 +0800

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -18,52 +18,67 @@ pub use strict_math::tanh;
 mod test {
     use super::*;
 
+    macro_rules! assert_approx_eq {
+        ($a:expr, $b:expr) => {{
+            let (a, b) = (&$a, &$b);
+            assert!(
+                (*a - *b).abs() < 1.0e-6,
+                "{} is not approximately equal to {}",
+                *a,
+                *b
+            );
+        }};
+    }
+
     #[test]
     pub fn test() {
-        assert_eq!(
+        assert_approx_eq!(
             acos(0.055605003447049994_f64),
             0.055605003447049994_f64.acos()
         );
-        assert_eq!(
+        assert_approx_eq!(
             asin(0.055605003447049994_f64),
             0.055605003447049994_f64.asin()
         );
-        assert_eq!(
+        assert_approx_eq!(
             atan(0.055605003447049994_f64),
             0.055605003447049994_f64.atan()
         );
-        assert_eq!(atan2(3.0_f64, -3.0_f64), 3.0_f64.atan2(-3.0_f64));
-        assert_eq!(
+        assert_approx_eq!(atan2(3.0_f64, -3.0_f64), 3.0_f64.atan2(-3.0_f64));
+        assert_approx_eq!(
             cosh(0.055605003447049994_f64),
             0.055605003447049994_f64.cosh()
         );
-        assert_eq!(
+        assert_approx_eq!(
             expm1(0.055605003447049994_f64),
             0.055605003447049994_f64.exp_m1()
         );
-        assert_eq!(hypot(2.0_f64, 3.0_f64), 2.0_f64.hypot(3.0_f64));
-        assert_eq!(
+        assert_approx_eq!(hypot(2.0_f64, 3.0_f64), 2.0_f64.hypot(3.0_f64));
+        assert_approx_eq!(
             log1p(0.055605003447049994_f64),
             0.055605003447049994_f64.ln_1p()
         );
-        assert_eq!(
+        assert_approx_eq!(
             sinh(0.055605003447049994_f64),
             0.055605003447049994_f64.sinh()
         );
-        assert_eq!(
+        assert_approx_eq!(
             tan(0.055605003447049994_f64),
             0.055605003447049994_f64.tan()
         );
-        assert_eq!(
+        assert_approx_eq!(
             tanh(0.055605003447049994_f64),
             0.055605003447049994_f64.tanh()
         );
-        assert_eq!(log(0.055605003447049994_f64), 0.055605003447049994_f64.ln());
-        assert_eq!(
+        assert_approx_eq!(log(0.055605003447049994_f64), 0.055605003447049994_f64.ln());
+        assert_approx_eq!(
             sqrt(0.055605003447049994_f64),
             0.055605003447049994_f64.sqrt()
         );
-        eprintln!("cbrt:: {}", -0.055605003447049994_f64.cbrt());
+        assert_approx_eq!(
+            cbrt(0.055605003447049994_f64),
+            0.055605003447049994_f64.cbrt()
+        );
         assert_eq!(cbrt(-0.055605003447049994_f64), -0.3816845880251514);
     }
 }
